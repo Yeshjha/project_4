0123c4b6d2ada9e6b387d8b6820cdd58a399ec0a
Yesh jha <yeshjha12@gmail.com>
2025-07-21 08:27:20 +0000

    save this fie

---
+++ script.js
@@ -0,0 +1,8 @@
+let box = document.querySelector('#box_1');
+let resultBox = document.querySelector('#result_box');
+let calButton = document.querySelector('#cal_button');
+
+calButton.addEventListener('click', function(e){
+    
+})
+
