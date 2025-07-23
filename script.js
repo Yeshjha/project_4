1f9d8d90ba253651cf0007db33da4026979f41d8
Yesh jha <yeshjha12@gmail.com>
2025-07-23 20:43:05 +0530

    Update script.js
    save this file

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let box = document.querySelector('#box_1');
 let resultBox = document.querySelector('#result_box');
 let calButton = document.querySelector('#cal_button');
-let button = document.querySelectorAll('.buttons')
-
-button.forEach(function(clickButton){
-    clickButton.addEventListener('click',function(){
+let button = document.querySelectorAll('.buttons');
+let output = document.querySelector('#give_result');
+button.forEach(function (clickButton) {
+    clickButton.addEventListener('click', function (e) {
         resultBox.innerHTML += clickButton.textContent
-    })
-    let sum = clickButton + clickButton;
+    });
+});
+
+output.addEventListener('click', function (e) {
+    resultBox.innerHTML = eval(resultBox.innerHTML);
     
-    let output = document.querySelector('#give_result')
-    output.addEventListener('submit', function(){
-        resultBox.textContent = sum;
-    })
+});
+
+let clear_Output = document.getElementById('clear_result');
+
+clear_Output.addEventListener('click', function (e) {
+    resultBox.innerHTML = '';
 })
 
 
